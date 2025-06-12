45028b37a34564a7011423efc507113f645c2170
vinayjha4243 <vjha9119@gmail.com>
2025-06-12 12:20:46 +0530

    Quiz-App

---
+++ Quiz-App/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Quiz from './component/Quiz'
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <Quiz/>
+    </div>
+  )
+}
+
+export default App
+
